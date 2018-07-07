2c09750f198d70e716ebd210488a282a8ec7c4a1
hevi0 <hehvi0@gmail.com>
2018-07-07 09:50:53 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 export $(cat .env | grep -v ^# | xargs)
 
-ALLUXIO_HOME=$(pwd)/alluxio-${ALLUXIO_VER}
-SPARK_HOME=$(pwd)/spark-${SPARK_VER}-bin-hadoop${HADOOP_VER}
+ALLUXIO_HOME=$(pwd)/$(ls -d alluxio-$ALLUXIO_VER*/ | cut -f1 -d'/')
+SPARK_HOME=$(pwd)/$(ls -d spark-$SPARK_VER*/ | cut -f1 -d'/')
 
-${ALLUXIO_HOME}/bin/alluxio-start.sh local NoMount
+${ALLUXIO_HOME}/bin/alluxio-start.sh local
 
 sleep 3
 
 # Mount nested understorage drives
-$ALLUXIO_HOME/bin/alluxio fs mount --option aws.accessKeyId=$AWS_ACCESS_KEY_ID --option aws.secretKey=$AWS_SECRET_KEY /s3 $S3_FOLDER
+#$ALLUXIO_HOME/bin/alluxio fs mount --option aws.accessKeyId=$AWS_ACCESS_KEY_ID --option aws.secretKey=$AWS_SECRET_KEY /s3 $S3_FOLDER
 
 #$ALLUXIO_HOME/bin/alluxio fs mount /hd ~/
 
@@ -19,3 +19,6 @@ rm -f spark
 ln -s $ALLUXIO_HOME alluxio
 ln -s $SPARK_HOME spark
 
+mkdir -p /mnt/alluxio
+sudo chown -R $USER /mnt/alluxio
+$ALLUXIO_HOME/integration/fuse/bin/alluxio-fuse.sh mount /mnt/alluxio
