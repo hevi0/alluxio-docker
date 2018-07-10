f2bb2e6dcbcd23512c230c570e999c447988d3d5
David Hevron <hehvi0@gmail.com>
2018-07-10 16:26:18 -0400

    more fixes

--- conf.sh
+++ conf.sh
@@ -21,30 +21,24 @@ ALLUXIO_JAVA_OPTS="
 -Dfs.s3n.awsAccessKeyId=${AWS_ACCESS_KEY_ID}
 -Dfs.s3n.awsSecretAccessKey=${AWS_SECRET_KEY}
 -Dalluxio.master.hostname=${ALLUXIO_MASTER_HOSTNAME}
-"
-
-ALLUXIO_WORKER_JAVA_OPTS="
--Dalluxio.underfs.address=${ALLUXIO_UNDERFS_ADDRESS}
--Dalluxio.worker.memory.size=${ALLUXIO_WORKER_MEMORY_SIZE}
--Dalluxio.worker.tieredstore.levels=2
--Dalluxio.worker.tieredstore.level0.alias=MEM
--Dalluxio.worker.tieredstore.level0.dirs.path=/mnt/ramdisk
 -Dalluxio.worker.tieredstore.level0.dirs.quota=${ALLUXIO_WORKER_MEMORY_SIZE}
--Dalluxio.worker.tieredstore.level0.watermark.high.ratio=0.9 # 216GB * 0.9 ~ 200GB
--Dalluxio.worker.tieredstore.level0.watermark.low.ratio=0.75 # 216GB * 0.75 ~ 160GB
--Dalluxio.worker.tieredstore.level1.alias=SSD
--Dalluxio.worker.tieredstore.level1.dirs.path=/mnt/ssd1
--Dalluxio.worker.tieredstore.level1.dirs.quota=${ALLUXIO_SSD_SIZE}
--Dalluxio.worker.tieredstore.level1.watermark.high.ratio=0.9 # 216GB * 0.9 ~ 200GB
--Dalluxio.worker.tieredstore.level1.watermark.low.ratio=0.75 # 216GB * 0.75 ~ 160GB
-"
-
-ALLUXIO_USER_JAVA_OPTS="
--Dalluxio.user.file.readtype.default=CACHE_PROMOTE
--Dalluxio.user.file.writetype.default=MUST_CACHE
+-Dalluxio.worker.tieredstore.level1.dirs.quota=${ALLUXIO_WORKER_SSD_SIZE}
 "
 
 EOF
 
 cat <<EOF > ${ALLUXIO_HOME}/conf/alluxio-site.properties
+alluxio.worker.tieredstore.levels=2
+alluxio.worker.tieredstore.level0.alias=MEM
+alluxio.worker.tieredstore.level0.dirs.path=/mnt/ramdisk
+alluxio.worker.tieredstore.level0.watermark.high.ratio=0.9 # 216GB * 0.9 ~ 200GB
+alluxio.worker.tieredstore.level0.watermark.low.ratio=0.75 # 216GB * 0.75 ~ 160GB
+alluxio.worker.tieredstore.level1.alias=SSD
+alluxio.worker.tieredstore.level1.dirs.path=/mnt/ssd1
+alluxio.worker.tieredstore.level1.watermark.high.ratio=0.9 # 216GB * 0.9 ~ 200GB
+alluxio.worker.tieredstore.level1.watermark.low.ratio=0.75 # 216GB * 0.75 ~ 160GB
+
+alluxio.user.file.readtype.default=CACHE_PROMOTE
+alluxio.user.file.writetype.default=MUST_CACHE
+
 EOF
