e5d1d619440dbc8c6e43af99a64cf0001057ad3b
hevi0 <hehvi0@gmail.com>
2018-07-13 11:48:40 -0400

    Update alluxio-env.sh

--- docker/conf/alluxio-env.sh
+++ docker/conf/alluxio-env.sh
@@ -2,8 +2,6 @@ ALLUXIO_WORKER_MEMORY_SIZE=8GB
 
 ALLUXIO_JAVA_OPTS="
 -Dlog4j.configuration=file:/home/hevi/alluxio-docker/alluxio-1.6.1/conf/log4j.properties
--Dfs.s3n.awsAccessKeyId=AKIAJAPGP3KOWGOO4MCQ
--Dfs.s3n.awsSecretAccessKey=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
 -Dalluxio.master.hostname=localhost
 -Dalluxio.worker.tieredstore.level0.dirs.quota=8GB
 -Dalluxio.worker.tieredstore.level1.dirs.quota=64GB
